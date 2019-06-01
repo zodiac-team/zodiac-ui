89fde5ca5df7ecae93e3397ce7a833f7b543d3c2
stupidawesome <stupidawesome@zerocents.com.au>
2019-06-01 11:58:24 +0900

    docs: fix urls for github pages

--- apps/docs/src/app/shared/location.service.ts
+++ apps/docs/src/app/shared/location.service.ts
@@ -39,7 +39,7 @@ export class LocationService {
     // TODO: ignore if url-without-hash-or-search matches current location?
     go(url: string|null|undefined) {
         if (!url) { return; }
-        url = this.stripSlashes(url);
+        // url = this.stripSlashes(url);
         if (/^http/.test(url)) {
             // Has http protocol so leave the site
             this.goExternal(url);
@@ -49,7 +49,7 @@ export class LocationService {
             this.scrollService.removeStoredScrollPosition();
             this.goExternal(url);
         } else {
-            window.location.assign(url);
+            this.location.go(url);
             this.urlSubject.next(url);
         }
     }
